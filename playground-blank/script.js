13deb1aa5e550bcda9ee85254a4809034f3f8136
Power_tile <hejizheng2004@163.com>
2020-07-17 10:01:34 +0800

    add blank playground

---
+++ playground-blank/script.js
@@ -0,0 +1,18 @@
+let app = new Vue({
+    el: "#playground",
+    data: {
+        message: "Hello, World!",
+    },
+    mounted: function() {
+
+    },
+    watch: {
+        
+    },
+    computed: {
+        
+    },
+    methods: {
+
+    },
+});
