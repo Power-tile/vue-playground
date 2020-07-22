91bbc86b8c64f5daa44de5df205ca58f1208d6f0
Xiaoxi Xu <xiaoxixu@Xiaoxis-MacBook-Pro.local>
2020-07-23 00:43:33 +0800

    fixed formatting issues and bugs; added .DS_Store to .gitignore

--- challenges/list-add/Xiaoxi/script.js
+++ challenges/list-add/Xiaoxi/script.js
@@ -1,16 +1,15 @@
 let app = new Vue({
     el: "#playground",
     data: {
-        list : ["First Message Ever"],
-        new_item :"",
+        list: ["First Message Ever"],
+        new_item: "",
     },
-
     methods: {
-        add_to_list: function(){
-            if(this.new_item.trim()!=""){
+        add_to_list: function() {
+            if (this.new_item.trim() !== "") {
                 this.list.push(this.new_item.trim())
+                this.new_item = ""
             }
-            this.new_item = ""
         }
     },
 });
